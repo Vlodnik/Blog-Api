dc472389d4c0db6f6c23ace3a53ea4385e68f753
Evan Thompson <evanbrowningthompson@gmail.com>
2018-01-13 18:35:44 -0500

    changed config.js to use mLab database URL

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blogging-app';
+//exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blogging-app';
+exports.DATABASE_URL = 'mongodb://franky-johnson:mabelthecat123@ds155577.mlab.com:55577/blogging-api';
 exports.PORT = process.env.PORT || 8080;
